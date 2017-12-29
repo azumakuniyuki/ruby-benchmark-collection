75e8e2d674d259228384bbaecafc7f7ef518160e
azumakuniyuki <azumakuniyuki@gmail.com>
2017-12-29 14:08:27 +0900

    Add sprintf()

--- string-plus-vs-others.rb
+++ string-plus-vs-others.rb
@@ -1,15 +1,17 @@
 require 'benchmark'
 
-HowMany = 80000
+HowMany = 20000
 String0 = 'shironekonyaaaaan'
 String1 = 'sironeko'
 String2 = 'nyaaaaan'
 
 stringP = String1 + String2
 stringA = String1 << String2
+stringS = sprintf("%s%s", String1, String2)
 
 puts 'ok' if stringP == String0
 puts 'ok' if stringA == String0
+puts 'ok' if stringS == String0
 
 Benchmark.bmbm do |b|
   b.report('String#+') do
@@ -21,6 +23,12 @@ Benchmark.bmbm do |b|
   b.report('String#<<') do
     HowMany.times do
       stringA = String1 << String2
+    end
+  end
+
+  b.report('sprintf()') do
+    HowMany.times do
+      stringS = sprintf("%s%s", String1, String2)
     end
   end
 end
@@ -39,11 +47,14 @@ ruby string-plus-vs-others.rb  13.28s user 17.97s system 98% cpu 31.658 total
 
 ruby 2.4.3p205 (2017-12-14 revision 61247) [x86_64-darwin16]
 Rehearsal ---------------------------------------------
-String#+    0.110000   0.000000   0.110000 (  0.112906)
-String#<<   0.050000   0.000000   0.050000 (  0.055197)
------------------------------------- total: 0.160000sec
+String#+    0.020000   0.000000   0.020000 (  0.014798)
+String#<<   0.010000   0.000000   0.010000 (  0.007509)
+sprintf()   2.300000   1.260000   3.560000 (  3.573221)
+------------------------------------ total: 3.590000sec
 
                 user     system      total        real
-String#+   13.730000  19.020000  32.750000 ( 33.718283)
-String#<<   0.040000   0.000000   0.040000 (  0.036711)
-ruby string-plus-vs-others.rb  14.12s user 19.04s system 97% cpu 34.162 total
+String#+    0.880000   1.340000   2.220000 (  2.233472)
+String#<<   0.000000   0.000000   0.000000 (  0.007416)
+sprintf()   4.240000   2.180000   6.420000 (  6.466877)
+ruby string-plus-vs-others.rb  7.61s user 4.82s system 99% cpu 12.503 total
+
