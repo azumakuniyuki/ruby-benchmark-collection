60d07d1f1405da404289bdf403210dca72cab576
azumakuniyuki <azumakuniyuki@gmail.com>
2017-12-30 19:29:30 +0900

    Update benchmark codes and results

--- string-plus-vs-others.rb
+++ string-plus-vs-others.rb
@@ -1,60 +1,74 @@
 require 'benchmark'
 
-HowMany = 20000
-String0 = 'shironekonyaaaaan'
-String1 = 'sironeko'
-String2 = 'nyaaaaan'
+HowMany = 1000000
+string0 = 'nekonyaan'
+string1 = 'nyaan'
 
-stringP = String1 + String2
-stringA = String1 << String2
-stringS = sprintf("%s%s", String1, String2)
+stringP = 'neko'; stringP += string1
+stringA = 'neko'; stringA << string1
+stringC = 'neko'; stringC.concat(string1)
 
-puts 'ok' if stringP == String0
-puts 'ok' if stringA == String0
-puts 'ok' if stringS == String0
+puts 'ok' if stringP == string0
+puts 'ok' if stringA == string0
+puts 'ok' if stringC == string0
 
 Benchmark.bmbm do |b|
   b.report('String#+') do
     HowMany.times do
-      stringP = String1 + String2
+      stringP  = ''
+      stringP += 'neko'
+      stringP += string1
     end
   end
 
   b.report('String#<<') do
     HowMany.times do
-      stringA = String1 << String2
+      stringP  = ''
+      stringA << 'neko'
+      stringA << string1
     end
   end
 
-  b.report('sprintf()') do
+  b.report('String#concat') do
     HowMany.times do
-      stringS = sprintf("%s%s", String1, String2)
+      stringC = ''
+      stringC.concat('neko')
+      stringC.concat(string1)
     end
   end
 end
 
 __END__
-ruby 2.3.0p0 (2015-12-25 revision 53290) [x86_64-darwin15]
-Rehearsal ---------------------------------------------
-String#+    0.060000   0.000000   0.060000 (  0.062340)
-String#<<   0.030000   0.000000   0.030000 (  0.037969)
------------------------------------- total: 0.090000sec
-
-                user     system      total        real
-String#+   13.010000  17.950000  30.960000 ( 31.315896)
-String#<<   0.020000   0.000000   0.020000 (  0.027570)
-ruby string-plus-vs-others.rb  13.28s user 17.97s system 98% cpu 31.658 total
-
+B=./string-plus-vs-others.rb; ruby -v; ruby $B; echo; R=/usr/local/jr/bin/jruby; $R -v; $R $B
 ruby 2.4.3p205 (2017-12-14 revision 61247) [x86_64-darwin16]
-Rehearsal ---------------------------------------------
-String#+    0.020000   0.000000   0.020000 (  0.014798)
-String#<<   0.010000   0.000000   0.010000 (  0.007509)
-sprintf()   2.300000   1.260000   3.560000 (  3.573221)
------------------------------------- total: 3.590000sec
+ok
+ok
+ok
+Rehearsal -------------------------------------------------
+String#+        1.140000   0.010000   1.150000 (  1.179719)
+String#<<       0.980000   0.010000   0.990000 (  0.989404)
+String#concat   1.720000   0.010000   1.730000 (  1.731737)
+---------------------------------------- total: 3.870000sec
 
-                user     system      total        real
-String#+    0.880000   1.340000   2.220000 (  2.233472)
-String#<<   0.000000   0.000000   0.000000 (  0.007416)
-sprintf()   4.240000   2.180000   6.420000 (  6.466877)
-ruby string-plus-vs-others.rb  7.61s user 4.82s system 99% cpu 12.503 total
+                    user     system      total        real
+String#+        1.110000   0.000000   1.110000 (  1.112322)
+String#<<       0.910000   0.010000   0.920000 (  0.916289)
+String#concat   1.720000   0.010000   1.730000 (  1.729537)
+ruby $B  7.70s user 0.06s system 99% cpu 7.840 total
+
+jruby 9.1.15.0 (2.3.3) 2017-12-07 929fde8 Java HotSpot(TM) 64-Bit Server VM 25.77-b03 on 1.8.0_77-b03 +jit [darwin-x86_64]
+ok
+ok
+ok
+Rehearsal -------------------------------------------------
+String#+        1.530000   0.050000   1.580000 (  0.553956)
+String#<<       0.740000   0.030000   0.770000 (  0.348538)
+String#concat   0.610000   0.020000   0.630000 (  0.368605)
+---------------------------------------- total: 2.980000sec
+
+                    user     system      total        real
+String#+        0.180000   0.000000   0.180000 (  0.174718)
+String#<<       0.220000   0.030000   0.250000 (  0.207940)
+String#concat   0.460000   0.010000   0.470000 (  0.342281)
+$R $B  11.10s user 0.50s system 227% cpu 5.110 total
 
