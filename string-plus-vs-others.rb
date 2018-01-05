2366bd44d16fb9fcc5971f8c6ebd7379e4e82954
azumakuniyuki <azumakuniyuki@gmail.com>
2018-01-05 13:10:37 +0900

    Update test codes and results

--- string-plus-vs-others.rb
+++ string-plus-vs-others.rb
@@ -1,68 +1,75 @@
 require 'benchmark'
 
-HowMany = 1000000
+HowMany = 2000000
 string0 = 'nekonyaan'
 string1 = 'nyaan'
 
 stringP = 'neko'; stringP += string1
 stringA = 'neko'; stringA << string1
 stringC = 'neko'; stringC.concat(string1)
+stringS = 'neko'; stringS = sprintf("%s%s", stringS, string1)
 
 puts 'ok' if stringP == string0
 puts 'ok' if stringA == string0
 puts 'ok' if stringC == string0
+puts 'ok' if stringS == string0
 
 Benchmark.bmbm do |b|
   b.report('String#+') do
     HowMany.times do
       stringP  = ''
-      stringP += 'neko'
-      stringP += string1
+      stringP += 'neko' + string1
     end
   end
 
   b.report('String#<<') do
     HowMany.times do
       stringA  = ''
-      stringA << 'neko'
-      stringA << string1
+      stringA << 'neko' << string1
     end
   end
 
   b.report('String#concat') do
     HowMany.times do
       stringC = ''
-      stringC.concat('neko')
-      stringC.concat(string1)
+      stringC.concat('neko' + string1)
+    end
+  end
+
+  b.report('String#sprintf') do
+    HowMany.times do
+      stringS = ''
+      stringS = sprintf("neko%s", string1)
     end
   end
 end
 
 __END__
 ruby 2.4.3p205 (2017-12-14 revision 61247) [x86_64-darwin16]
-Rehearsal -------------------------------------------------
-String#+        1.130000   0.000000   1.130000 (  1.142643)
-String#<<       0.920000   0.000000   0.920000 (  0.928554)
-String#concat   1.680000   0.010000   1.690000 (  1.686617)
----------------------------------------- total: 3.740000sec
+Rehearsal --------------------------------------------------
+String#+         2.130000   0.000000   2.130000 (  2.143257)
+String#<<        1.740000   0.010000   1.750000 (  1.750788)
+String#concat    2.710000   0.000000   2.710000 (  2.722870)
+String#sprintf   3.450000   0.010000   3.460000 (  3.461874)
+---------------------------------------- total: 10.050000sec
 
-                    user     system      total        real
-String#+        1.100000   0.000000   1.100000 (  1.103764)
-String#<<       0.890000   0.000000   0.890000 (  0.898239)
-String#concat   1.650000   0.000000   1.650000 (  1.652806)
+                     user     system      total        real
+String#+         2.120000   0.010000   2.130000 (  2.139258)
+String#<<        1.750000   0.000000   1.750000 (  1.763009)
+String#concat    2.670000   0.010000   2.680000 (  2.690509)
+String#sprintf   3.390000   0.010000   3.400000 (  3.412469)
 
 jruby 9.1.15.0 (2.3.3) 2017-12-07 929fde8 Java HotSpot(TM) 64-Bit Server VM 25.77-b03 on 1.8.0_77-b03 +jit [darwin-x86_64]
-ok
-ok
-ok
-Rehearsal -------------------------------------------------
-String#+        1.920000   0.060000   1.980000 (  0.666109)
-String#<<       0.760000   0.020000   0.780000 (  0.397076)
-String#concat   0.590000   0.030000   0.620000 (  0.360320)
----------------------------------------- total: 3.380000sec
+Rehearsal --------------------------------------------------
+String#+         1.910000   0.060000   1.970000 (  0.820285)
+String#<<        1.070000   0.040000   1.110000 (  0.681074)
+String#concat    0.750000   0.010000   0.760000 (  0.527113)
+String#sprintf   2.120000   0.060000   2.180000 (  1.027155)
+----------------------------------------- total: 6.020000sec
 
-                    user     system      total        real
-String#+        0.220000   0.000000   0.220000 (  0.214419)
-String#<<       0.260000   0.020000   0.280000 (  0.272481)
-String#concat   0.280000   0.000000   0.280000 (  0.271635)
+                     user     system      total        real
+String#+         0.390000   0.010000   0.400000 (  0.380950)
+String#<<        0.560000   0.010000   0.570000 (  0.496603)
+String#concat    0.450000   0.000000   0.450000 (  0.446095)
+String#sprintf   1.370000   0.020000   1.390000 (  0.803323)
 
